c133692f87d7cde3db0fc70d345167682df3d5b1
princeHan0 <100517943+princeHan0@users.noreply.github.com>
2025-03-06 05:12:13 +0000

    Add files via upload

--- ASE coffeeshop/src/main/Order.java
+++ ASE coffeeshop/src/main/Order.java
@@ -3,6 +3,12 @@ package main;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * The class contains details about an order. 
+ * Each order contains details such as order ID, customer ID, 
+ * item ID, item name, item price, and timestamp.
+ */
+
 public class Order {
 
 	private Integer orderID;
@@ -12,8 +18,10 @@ public class Order {
 	private Float itemPrice;
 	private LocalDateTime currTime;
 	
+	// Formatter for converting between LocalDateTime and String
 	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
+	// Getter and Setter methods
 	public Integer getOrderID() {
 		return orderID;
 	}
@@ -56,9 +64,20 @@ public class Order {
 		this.currTime = currTime;
 	}
 	
+	/**
+     * Sets the current time from a formatted string.
+     *
+     * @param timeStr The time string in the format "yyyy/MM/dd HH:mm:ss".
+     */
     public void setCurrTimeFromString(String timeStr) {
         this.currTime = LocalDateTime.parse(timeStr, formatter);
     }
+    
+    /**
+     * Returns the current time as a formatted string.
+     *
+     * @return The formatted time string in the format "yyyy/MM/dd HH:mm:ss".
+     */
     public String getCurrTimeAsString() {
         return currTime.format(formatter);
     }
@@ -70,6 +89,7 @@ public class Order {
 				+ ", Item Name: " + itemName + ", Item Price: " + itemPrice +  ", Timestamp: " + getCurrTimeAsString();
 	}	
 	
+	// Returns a CSV-formatted string representation of the order.
 	public String toCSVString() {
         return String.join(",", orderID.toString(), customerID, itemID, itemName, itemPrice.toString(), getCurrTimeAsString());
     }
