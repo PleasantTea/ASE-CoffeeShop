0aeaba94e0c29484f39c197b3d39ddd0ec2e3c0e
princeHan0 <100517943+princeHan0@users.noreply.github.com>
2025-03-20 23:18:20 +0000

    Update Main.java for adding customerQueue

--- ASE coffeeshop/src/main/Main.java
+++ ASE coffeeshop/src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import fileRead.MenuFileRead;
 import fileRead.OrdersFileRead;
+import model.CustomerQueue;
 import exception.InvalidMenuFileReadException;
 import exception.InvalidOrdersFileReadException;
 
@@ -14,15 +15,18 @@ public class Main {
     private static String ordersFileName = "ASE coffeeshop/src/Orders.csv";
     private static MenuFileRead menuFileReader;
     private static OrdersFileRead ordersFileReader;
+    private static CustomerQueue customerQueue;
     private static Basket basket;
 
     public static void main(String[] args) throws InvalidMenuFileReadException, InvalidOrdersFileReadException {
         // Initialize file management
         menuFileReader = new MenuFileRead();
         ordersFileReader = new OrdersFileRead();
+        customerQueue = CustomerQueue.getInstance();
         
         menuFileReader.readCSVAndStoreData(menuFileName);
         ordersFileReader.readCSVAndStoreData(ordersFileName);
+        customerQueue.addExistingCustomer();
         
         basket = new Basket();
         
