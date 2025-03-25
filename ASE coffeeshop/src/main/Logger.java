54ba541d63581208ab2baf93125c83ec8fd28d43
15692327731 <xf6270110@163.com>
2025-03-25 16:06:51 +0000

    Add files via upload

---
+++ ASE coffeeshop/src/main/Logger.java
@@ -0,0 +1,85 @@
+package main;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+
+public class Logger {
+	 private static volatile Logger instance;
+	    private static Object mutex = new Object();
+	    private boolean printed;
+	    String logstring = "";
+
+	    /**
+	     * Sets printed to false;
+	     */
+	    private Logger() {
+	        printed = false;
+	    }
+
+	    
+	    /**
+	     * Constructor of the logger class. Creates a new singleton instance of logger only if there is no other active logger.
+	     * @return returns the logger singleton.
+	     */
+	    /*
+	    public static Logger getInstance() {
+	        Logger result = instance;
+	        if (result == null) {
+	            synchronized (mutex) {
+	                result = instance;
+	                if (result == null)
+	                    instance = result = new Logger();
+	            }
+	        }
+	        return result;
+	    }*/
+	    public static Logger getInstance() {
+	        if (instance == null) {
+	            synchronized (mutex) {
+	                if (instance == null)
+	                    instance = new Logger();
+	            }
+	        }
+	        return instance;
+	    }
+
+
+	    /**
+	     * Adds messages passed to it to a string in the logger.
+	     * @param message Is the message passed to the logger.
+	     */
+	    
+	    public void info(String message) {
+	        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+	        String a = timestamp + ": " + message + "\n";
+	        System.out.println(a);
+	        logstring += a;
+	    }
+	  
+	    /**
+	     * @return Returns if the logger has already printed the log. This method can only be accessed by one thread at at time.
+	     */
+	    public synchronized boolean print() {
+	        if (!printed) {
+	            printed = true;
+	            return true;
+	        }
+	        return false;
+	    }
+
+	    /**
+	     * Prints the messages stored in the log to the Log file.
+	     */
+	    public void printFile() {
+	        try {
+	            System.out.println("PRINTED");
+	            PrintWriter out = new PrintWriter(new FileWriter("Files/LoggerFile.txt", true)); // Supplementary mode
+	            out.println(logstring);
+	            out.close();
+	            System.exit(0);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+}
