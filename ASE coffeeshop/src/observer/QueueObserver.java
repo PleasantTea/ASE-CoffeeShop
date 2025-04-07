c2c3d1e6595a566a8ff99d9fe70aa8e8f9f42d1c
15692327731 <xf6270110@163.com>
2025-04-07 11:48:05 +0100

    Add files via upload

--- ASE coffeeshop/src/observer/QueueObserver.java
+++ ASE coffeeshop/src/observer/QueueObserver.java
@@ -1,1 +1,15 @@
+package observer;
 
+
+/**
+ * QueueObserver is an observer interface
+ * Used to receive notifications and update the interface when the CustomerQueue changes
+ */
+public interface QueueObserver {
+	/**
+     * When the customer queue changes (a customer joins or leaves the queue)
+     * CustomerQueue will call this method to notify all observers to update the display
+     */
+	void updateQueueDisplay();
+
+}
